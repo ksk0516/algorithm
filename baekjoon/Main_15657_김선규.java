59c3d5690f3304a9b7e22372124c73decb191d1f
seonkyu kim <iwon4u@naver.com>
2022-08-16 22:19:18 +0900

    20220816

---
+++ baekjoon/Main_15657_김선규.java
@@ -0,0 +1,50 @@
+package a0816;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_15657_김선규 {
+
+	static int N, M;
+	static int[] ans, arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		ans = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		// input end
+		
+		Arrays.sort(arr);
+		
+		perm(0, 0);
+		
+		System.out.println(sb);
+	}
+
+	public static void perm(int idx, int cnt) {
+		if(cnt == M) {
+			for(int i : ans)
+				sb.append(i+" ");
+			sb.append('\n');
+			
+			return;
+		}
+		
+		for(int i=idx; i<N; i++) {
+			ans[cnt] = arr[i];
+			perm(i, cnt+1);
+		}
+	}
+}
